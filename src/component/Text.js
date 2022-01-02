07bf0ee53fb00eaa5dc4cf2f42e41d323d59f1a9
shikubro90 <shikubro90@gmail.com>
2022-01-02 15:36:49 +0600

    composite

--- src/component/Text.js
+++
@@ -1,9 +0,0 @@
-export default function Text({addEmoji}){
-
-    return 
-        const text = "This is javaScript program";
-        
-        return <div>{addEmoji ? addEmoji(text, '💖') : text }</div>
-    
-
-}
